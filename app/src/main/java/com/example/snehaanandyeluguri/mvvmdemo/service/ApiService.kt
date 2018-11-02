f935eb6a6fe8833fe1a05f3928e05cd28a1d2ac5
Sneha Anand <snehareddy048@gmail.com>
2018-11-01 23:44:55 -0400

    working on adding live data

--- app/src/main/java/com/example/snehaanandyeluguri/mvvmdemo/service/ApiService.kt
+++ app/src/main/java/com/example/snehaanandyeluguri/mvvmdemo/service/ApiService.kt
@@ -1,19 +1,20 @@
 package com.example.snehaanandyeluguri.mvvmdemo.service
 
+import android.arch.lifecycle.MutableLiveData
 import com.example.snehaanandyeluguri.mvvmdemo.model.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import io.reactivex.Observable
+//import io.reactivex.Observable
 
 interface ApiService {
 
 //     val BASE_URL="https://beta.ourmanna.com/api/v1/get/?format=json"
 
 @GET("v1/get/")
-fun getVerse(@Query("format") format: String): Observable<Response>
+fun getVerse(@Query("format") format: String): MutableLiveData<Response>
 
     companion object Factory{
         fun create():ApiService{
